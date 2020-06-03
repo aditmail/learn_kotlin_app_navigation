ca65bdb64796e91ab35ca40bf59e5afc7e2eea25
aditya ismail <aditmail321@gmail.com>
2020-06-03 17:47:45 +0700

    Init Data

---
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.navigation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.example.android.navigation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    //private lateinit var appBarConfig: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout //DrawerLayout Setting
+
+        val navController = this.findNavController(R.id.fragmentMain) //set controller first
+        //appBarConfig = AppBarConfiguration(navController.graph, drawerLayout)
+
+        //Set NavDrawable to LOCKED if Fragment gone from the startDestination -> Title Fragment
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            if (destination.id == controller.graph.startDestination) {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            } else {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout) //Setup for Action Button...? and Drawer Layout
+        NavigationUI.setupWithNavController(binding.navView, navController) //Setup for Nav Drawable
+    }
+
+    override fun onSupportNavigateUp(): Boolean { //Go to where if clicked.
+        val navController = this.findNavController(R.id.fragmentMain)
+        //return navController.navigateUp() //for About Only.. one drawable at actionbar
+        return NavigationUI.navigateUp(navController, drawerLayout) //Nav Drawable and Action Bar
+    }
+}
